e093ed4a87cadd36a94a4f3c309e301c23d4ff86
harshaneo17 <harsha.god86@gmail.com>
2021-07-13 14:27:25 +0100

    Add files via upload

---
+++ QUBEServo2.h
@@ -0,0 +1,19 @@
+
+
+#ifndef QUBEServo2_h
+#define QUBEServo2_h
+
+#include <Arduino.h>
+
+class Display
+{
+public:
+	Display(); //Build the display class
+	void buildString(float theta, float alpha, float currentSense, int moduleID, int moduleStatus); //Create the data string
+	String dData;  // string that will be printed to the Arduino Serial Monitor
+	boolean dDataReady;  // true when there is a string ready to be printed
+	int dDataIndex;  // used to print the string one character at a time
+private:
+	void reset(int moduleID, int moduleStatus); //reset the data string
+};
+#endif
